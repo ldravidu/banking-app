179e3c1854daff49767efa989ed4ece4fe63d385
ldravidu-wsl <ldravidu@gmail.com>
2025-04-01 23:51:01 +0530

    Implement home page layout with user greeting and total balance display; add AnimatedCounter, DoughnutChart, HeaderBox, and TotalBalanceBox components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "John", lastName: "Doe" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Hello"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your accounts and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1248.38}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
